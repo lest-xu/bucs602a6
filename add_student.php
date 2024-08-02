46d843e2f77dc7a9c7921cba2f4bf1675ba2ed47
lest-xu <autodataxx@gmail.com>
2024-08-02 02:22:59 -0400

    update

--- add_student.php
+++ add_student.php
@@ -1,38 +1,36 @@
 <?php
-    
-    require_once('database.php');
 
-    // Get the student form data
-    // get student information from the form input
-    $courseID = filter_input(INPUT_POST, 'courseID');
-    $firstName = filter_input(INPUT_POST, 'first_name');
-    $lastName = filter_input(INPUT_POST, 'last_name');
-    $email = filter_input(INPUT_POST, 'email');
+require_once('database.php');
 
-    //check if the input is valid
-    if (!$courseID || !$firstName ||! $lastName || !$email) {
-        // show error msg when missing value or invalid of the input
-        $error = 'Invalid student information, please try again!';
-        // display error page
-        include('error.php');
-    } else {
-        // Add the student to the database  
-        // insert query for adding a student record
-        $queryInsertStudent = 'INSERT INTO 
+// Get the student form data
+// get student information from the form input
+$courseID = filter_input(INPUT_POST, 'courseID');
+$firstName = filter_input(INPUT_POST, 'first_name');
+$lastName = filter_input(INPUT_POST, 'last_name');
+$email = filter_input(INPUT_POST, 'email');
+
+//check if the input is valid
+if (!$courseID || !$firstName || !$lastName || !$email) {
+    // show error msg when missing value or invalid of the input
+    $error = 'Invalid student information, please try again!';
+    // display error page
+    include('error.php');
+} else {
+    // Add the student to the database  
+    // insert query for adding a student record
+    $queryInsertStudent = 'INSERT INTO 
                                     sk_students (courseID, firstName, lastName, email)
                                 VALUES
                                     (:courseID, :firstName, :lastName, :email)';
-        $statementStudent = $db->prepare($queryInsertStudent);
-        $statementStudent->bindvalue(':courseID', $courseID);
-        $statementStudent->bindvalue(':firstName', $firstName);
-        $statementStudent->bindvalue(':lastName', $lastName);
-        $statementStudent->bindvalue(':email', $email);
-        $statementStudent->execute();
-        $statementStudent->closeCursor();
+    $statementStudent = $db->prepare($queryInsertStudent);
+    $statementStudent->bindvalue(':courseID', $courseID);
+    $statementStudent->bindvalue(':firstName', $firstName);
+    $statementStudent->bindvalue(':lastName', $lastName);
+    $statementStudent->bindvalue(':email', $email);
+    $statementStudent->execute();
+    $statementStudent->closeCursor();
 
-        // Display the Student List page
-        include('index.php');
-
-    }
-    
-?>
+    // redirect to the student list page
+    header('Location: index.php');
+    exit();
+}
